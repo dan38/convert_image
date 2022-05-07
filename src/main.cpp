f639c86147439582db227f5c54bd520b4b742d5f
dchen <dchen3899@gmail.com>
2022-05-06 22:19:53 -0700

    Attempting to try different method of creating objects, not looking good so far

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "commands.h"
 #include "ConvertImage.h"
+#include "Action.h"
+#include "CreateAction.h"
 
 using namespace cv;
 using namespace std;
@@ -86,9 +88,121 @@ void test_lines()
     }
 }
 
+// Action *create_action(char const *line)
+Action *create_action()
+{
+    const char *line = "blah";
+    Action *action = NULL;
+    char *s = strdup(line);
+    char *p = strtok(s, " ,");
+    enum enum_commands command;
+    // std::string target_name;
+    // std::vector<std::string> items;
+    // items.clear();
+    // target_name.clear();
+
+    if (p[0] == '#')
+    {
+        command = eCOMMENT;
+    }
+    else
+    {
+        while (p)
+        {
+            if (strcmp(p, "save") == 0)
+            {
+                command = eSAVE;
+                // func = save;
+            }
+            else if (strcmp(p, "load") == 0)
+            {
+                command = eLOAD;
+                // func = load;
+            }
+            else if (strcmp(p, "create") == 0)
+            {
+                command = eCREATE;
+                // TODO what if two commands on a line?
+                action = new CreateAction();
+                action->name();
+                action->set_command(line);
+                //  func = create;
+            }
+            else if (strcmp(p, "rotate") == 0)
+            {
+                command = eROTATE;
+                // func = rotate;
+            }
+            else if (strcmp(p, "line") == 0)
+            {
+                command = eLINE;
+            }
+            else if (strcmp(p, "circle") == 0)
+            {
+                command = eCIRCLE;
+            }
+            else if (strcmp(p, "rectangle") == 0)
+            {
+                command = eRECTANGLE;
+            }
+            else if (p[0] == '=')
+            {
+                // target_name = items.front().c_str();
+                // items.clear();
+                // assert(items.size() == 0);
+            }
+            else
+            {
+                // items.push_back(std::string(p));
+            }
+
+            p = strtok(NULL, " ,");
+        }
+    }
+    free(s);
+    return action;
+}
+
+void test_lines_2()
+{
+    // create height x width image
+    // color is BGR
+    const char *lines[] = {
+        "srcb = create 400, 100, 255, 0, 0",
+        "srcg = create 100, 200, 0, 255, 0",
+        "srcr = create 300, 200, 0, 0, 255",
+        "line srcb, 20, 30, 100, 50, 255, 255, 0, 4",
+        "rotate srcb, 45, 10, 20",
+        "display result",
+        "exit"};
+    std::map<std::string, cv::Mat> images;
+    char step[100] = {};
+    Action *action = NULL;
+
+    for (int ii = 0; ii < sizeof(lines) / sizeof(char *); ii++)
+    {
+        printf("%s\n", lines[ii]);
+        // action = create_action(lines[ii]);
+        action = create_action();
+        // cmd->set_command(lines[ii]);
+        //  cmd->debug_dump();
+        snprintf(step, 99, "%d", ii);
+        // ci->do_command(cmd, step);
+        if (action)
+        {
+            // this is kinda silly to allocate and then delete the action
+            // must be a better way to do it
+            // maybe don't delete the action?
+            action->render(images);
+            delete (action);
+        }
+        printf("==================\n");
+    }
+}
+
 int main(int argv, char *args[])
 {
-    test_lines();
+    test_lines_2();
     if (argv < 2)
     {
         printf("Usage: %s <file>\n", args[0]);
